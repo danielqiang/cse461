bade3db2c485cebb00346f3c380cfd0e23457094
Daniel Qiang <daniel_qiang@hotmail.com>
2020-04-09 18:02:59 -0700

    delete unnecessary files

--- cse461/runclient.py
+++
@@ -1,5 +0,0 @@
-from project1.client import Client
-
-if __name__ == '__main__':
-    with Client(student_id=592) as client:
-        client.start()
--- cse461/runserver.py
+++
@@ -1,4 +0,0 @@
-from project1.server import Server
-
-if __name__ == '__main__':
-    Server().run(seconds=1)
