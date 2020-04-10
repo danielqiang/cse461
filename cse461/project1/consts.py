d6ef4d905e5ed701e9cee764b46de9a75917c91d
Daniel Qiang <daniel_qiang@hotmail.com>
2020-04-09 19:01:21 -0700

    Refactoring

--- cse461/project1/consts.py
+++ cse461/project1/consts.py
@@ -1,6 +1,7 @@
-__all__ = ['IP_ADDR', 'START_PORT', 'STUDENT_ID']
+__all__ = ['IP_ADDR', 'START_PORT', 'STUDENT_ID', 'TIMEOUT']
 
-# IP_ADDR = 'localhost'
-IP_ADDR = 'attu3.cs.washington.edu'
+IP_ADDR = 'localhost'
+# IP_ADDR = 'attu3.cs.washington.edu'
 START_PORT = 12235
 STUDENT_ID = 592
+TIMEOUT = None
